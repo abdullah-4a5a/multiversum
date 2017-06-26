598c102cf2f9c9da1ad792ee0cf409cfeac14e9d
abdullah-4a5a <501419@edu.rocmn.nl>
2017-06-26 11:47:05 +0200

    Delete iframe.php

--- webshop/sql/iframe.php
+++
@@ -1,8 +0,0 @@
-<doctype! html>
-<head>
-</head>
-<body>
-<p data-height="265" data-theme-id="0" data-slug-hash="bqWLaM" data-default-tab="css,result" data-user="mandymichael" data-embed-version="2" data-pen-title="Awkward Zebra Text Effect" class="codepen">See the Pen <a href="https://codepen.io/mandymichael/pen/bqWLaM/">Awkward Zebra Text Effect</a> by Mandy Michael (<a href="http://codepen.io/mandymichael">@mandymichael</a>) on <a href="http://codepen.io">CodePen</a>.</p>
-<script async src="https://production-assets.codepen.io/assets/embed/ei.js"></script>
-</body>
-</html>
